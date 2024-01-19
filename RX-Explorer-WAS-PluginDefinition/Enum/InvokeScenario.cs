3b38768e797e21502d8e077d0ab4a7964f9dcd9e
Ruofan <zrfcfgs@outlook.com>
2024-01-19 10:46:56 +0800

    Add a new scenario to the plugin definition

--- RX-Explorer-WAS-PluginDefinition/Enum/InvokeScenario.cs
+++ RX-Explorer-WAS-PluginDefinition/Enum/InvokeScenario.cs
@@ -13,13 +13,12 @@ namespace RX_Explorer_WAS.PluginDefinition.Enum
         /// Do not invoke on any scenario.
         /// </summary>
         /// <remarks>
-        /// Default value and plugin developer should not use this flag on any situation.<br/>
-        /// Feature that use this value will never be invoked in any scenario.
+        /// Indicator should not invoke automatically in any scenario.
         /// </remarks>
         None = 0,
 
         /// <summary>
-        /// Invoke the feature on application launch.
+        /// Invoke the feature on application launch. This scenario will only be invoked when <see cref="FeatureStatus"/> is <see cref="FeatureStatus.Active"/>
         /// </summary>
         /// <remarks>
         /// Host will invoke those plugin through <see cref="IInvokablePluginComponent.InvokePluginFeatureAsync(IPluginFeatureInformation, FeatureStatus, object[])"/> and would ignore any return value.
@@ -27,7 +26,7 @@ namespace RX_Explorer_WAS.PluginDefinition.Enum
         Launch = 1,
 
         /// <summary>
-        /// Invoke the feature on application shutdown.
+        /// Invoke the feature on application shutdown. This scenario will only be invoked when <see cref="FeatureStatus"/> is <see cref="FeatureStatus.Active"/>
         /// </summary>
         /// <remarks>
         /// Host will invoke those plugin through <see cref="IInvokablePluginComponent.InvokePluginFeatureAsync(IPluginFeatureInformation, FeatureStatus, object[])"/> and would ignore any return value.
@@ -35,13 +34,21 @@ namespace RX_Explorer_WAS.PluginDefinition.Enum
         Shutdown = 2,
 
         /// <summary>
-        /// Invoke the feature on application needs to elevate itself.
+        /// Invoke the feature on application needs to elevate itself. This scenario will only be invoked when <see cref="FeatureStatus"/> is <see cref="FeatureStatus.Active"/>
         /// </summary>
         /// <remarks>
         /// Host will invoke those plugin through <see cref="IInvokablePluginComponent{T}.InvokePluginFeatureAsync(IPluginFeatureInformation, FeatureStatus, object[])"/>.<br/>
         /// Especially, the plugin developer should not set <see cref="IPluginFeatureInformation.IsElevationRequired"/> to <see langword="true"/>.<br/>
         /// Plugin developer should return <see cref="Process"/> from <see cref="IInvokablePluginComponent{T}.InvokePluginFeatureAsync(IPluginFeatureInformation, FeatureStatus, object[])"/>.
         /// </remarks>
-        Elevation = 4
+        Elevation = 4,
+
+        /// <summary>
+        /// Invoke the feature on feature status changed. This scenario will be invoked when <see cref="FeatureStatus"/> switching between <see cref="FeatureStatus.Active"/> and <see cref="FeatureStatus.Deactive"/>
+        /// </summary>
+        /// <remarks>
+        /// Host will invoke those plugin through <see cref="IInvokablePluginComponent.InvokePluginFeatureAsync(IPluginFeatureInformation, FeatureStatus, object[])"/> and would ignore any return value.
+        /// </remarks>
+        FeatureStatusChanged = 8
     }
 }
