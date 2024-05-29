6aa297ef8dd3e1cfda08fd982805f283c00644cb
Ruofan <zrfcfgs@outlook.com>
2024-05-29 15:57:37 +0800

    Update the incorrect API xml documents

--- RX-Explorer-WAS-PluginDefinition/Enum/InvokeScenario.cs
+++ RX-Explorer-WAS-PluginDefinition/Enum/InvokeScenario.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Threading;
 
 namespace RX_Explorer_WAS.PluginDefinition.Enum
 {
@@ -21,7 +23,7 @@ namespace RX_Explorer_WAS.PluginDefinition.Enum
         /// Invoke the feature on application launch. This scenario will only be invoked when <see cref="FeatureStatus"/> is <see cref="FeatureStatus.Active"/>
         /// </summary>
         /// <remarks>
-        /// Host will invoke those plugin through <see cref="IInvokablePluginComponent.InvokePluginFeatureAsync(IPluginFeatureInformation, FeatureStatus, object[])"/> and would ignore any return value.
+        /// Host will invoke those plugin through <see cref="IInvokablePluginComponent.InvokeFeatureAsync(IPluginFeatureInformation, FeatureStatus, IEnumerable{object}, CancellationToken)"/> and would ignore any return value.
         /// </remarks>
         Launch = 1,
 
@@ -29,7 +31,7 @@ namespace RX_Explorer_WAS.PluginDefinition.Enum
         /// Invoke the feature on application shutdown. This scenario will only be invoked when <see cref="FeatureStatus"/> is <see cref="FeatureStatus.Active"/>
         /// </summary>
         /// <remarks>
-        /// Host will invoke those plugin through <see cref="IInvokablePluginComponent.InvokePluginFeatureAsync(IPluginFeatureInformation, FeatureStatus, object[])"/> and would ignore any return value.
+        /// Host will invoke those plugin through <see cref="IInvokablePluginComponent.InvokeFeatureAsync(IPluginFeatureInformation, FeatureStatus, IEnumerable{object}, CancellationToken)"/> and would ignore any return value.
         /// </remarks>
         Shutdown = 2,
 
@@ -37,9 +39,9 @@ namespace RX_Explorer_WAS.PluginDefinition.Enum
         /// Invoke the feature on application needs to elevate itself. This scenario will only be invoked when <see cref="FeatureStatus"/> is <see cref="FeatureStatus.Active"/>
         /// </summary>
         /// <remarks>
-        /// Host will invoke those plugin through <see cref="IInvokablePluginComponent{T}.InvokePluginFeatureAsync(IPluginFeatureInformation, FeatureStatus, object[])"/>.<br/>
+        /// Host will invoke those plugin through <see cref="IInvokablePluginComponent{T}.InvokeFeatureAsync(IPluginFeatureInformation, FeatureStatus, IEnumerable{object}, CancellationToken)"/>.<br/>
         /// Especially, the plugin developer should not set <see cref="IPluginFeatureInformation.IsElevationRequired"/> to <see langword="true"/>.<br/>
-        /// Plugin developer should return <see cref="Process"/> from <see cref="IInvokablePluginComponent{T}.InvokePluginFeatureAsync(IPluginFeatureInformation, FeatureStatus, object[])"/>.<br/><br/>
+        /// Plugin developer should return <see cref="Process"/> from <see cref="IInvokablePluginComponent{T}.InvokeFeatureAsync(IPluginFeatureInformation, FeatureStatus, IEnumerable{object}, CancellationToken)"/>.<br/><br/>
         /// Warning: Only the first plugin's feature with <see cref="InvokeScenario.Elevation"/> that return <see cref="Process"/> successfully will be used by host. Which means if multiple plugins provide the feature about <see cref="InvokeScenario.Elevation"/>, will use the first success one.
         /// </remarks>
         Elevation = 4,
@@ -48,7 +50,7 @@ namespace RX_Explorer_WAS.PluginDefinition.Enum
         /// Invoke the feature on feature status changed. This scenario will be invoked when <see cref="FeatureStatus"/> switching between <see cref="FeatureStatus.Active"/> and <see cref="FeatureStatus.Deactive"/>
         /// </summary>
         /// <remarks>
-        /// Host will invoke those plugin through <see cref="IInvokablePluginComponent.InvokePluginFeatureAsync(IPluginFeatureInformation, FeatureStatus, object[])"/> and would ignore any return value.
+        /// Host will invoke those plugin through <see cref="IInvokablePluginComponent.InvokeFeatureAsync(IPluginFeatureInformation, FeatureStatus, IEnumerable{object}, CancellationToken)"/> and would ignore any return value.
         /// </remarks>
         FeatureStatusChanged = 8
     }
