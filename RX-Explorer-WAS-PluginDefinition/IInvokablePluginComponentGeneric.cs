e08b9b0e44fedabe6f539565254743ed96904561
Ruofan <zrfcfgs@outlook.com>
2025-07-09 19:08:51 +0800

    Add IInvokablePluginComponent.InvokeFeatureAsync default implementation for IInvokablePluginComponent<T>. Add new property "AppMinVersion"

--- RX-Explorer-WAS-PluginDefinition/IInvokablePluginComponentGeneric.cs
+++ RX-Explorer-WAS-PluginDefinition/IInvokablePluginComponentGeneric.cs
@@ -17,15 +17,27 @@ namespace RX_Explorer_WAS.PluginDefinition
         /// <summary>
         /// Invoke the plugin features through this function and return the data that need to exchange with host.
         /// </summary>
-        /// <param name="Feature">Feature that would like to be invoked.</param>
-        /// <param name="Status">Feature status</param>
-        /// <param name="InputParameters">Invoke parameters.</param>
-        /// <param name="CancelToken">Cancellation token</param>
+        /// <param name="Feature">Feature that will be invoked.</param>
+        /// <param name="Status">Indicate that user active the feature or not.</param>
+        /// <param name="InputParameters">Parameters that would pass to the plugin during invoking. Optional.</param>
+        /// <param name="CancelToken">Cancellation token. Optional.</param>
         /// <remarks>
         /// Host will invoke the plugin and receive the result through this function, plugin developer should implement this function properly.<br/>
         /// Please make sure use <see langword="async"/> execution in this function even you do not actually need <see langword="async"/> so that it would not block the UI thread.<br/>
         /// For example: use <see cref="Task.Run(System.Action)"/> to warp the code you want to execute.
         /// </remarks>
         public new Task<T> InvokeFeatureAsync(IPluginFeatureInformation Feature, FeatureStatus Status, IEnumerable<object> InputParameters = null, CancellationToken CancelToken = default);
+
+        /// <summary>
+        /// Default implementation for <see cref="IInvokablePluginComponent.InvokeFeatureAsync(IPluginFeatureInformation, FeatureStatus, IEnumerable{object}, CancellationToken)"/> method.
+        /// </summary>
+        /// <param name="Feature">Feature that will be invoked.</param>
+        /// <param name="Status">Indicate that user active the feature or not.</param>
+        /// <param name="InputParameters">Parameters that would pass to the plugin during invoking. Optional.</param>
+        /// <param name="CancelToken">Cancellation token. Optional.</param>
+        Task IInvokablePluginComponent.InvokeFeatureAsync(IPluginFeatureInformation Feature, FeatureStatus Status, IEnumerable<object> InputParameters, CancellationToken CancelToken)
+        {
+            return InvokeFeatureAsync(Feature, Status, InputParameters, CancelToken);
+        }
     }
 }
--- RX-Explorer-WAS-PluginDefinition/IPluginComponent.cs
+++ RX-Explorer-WAS-PluginDefinition/IPluginComponent.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace RX_Explorer_WAS.PluginDefinition
 {
@@ -17,10 +18,23 @@ namespace RX_Explorer_WAS.PluginDefinition
         public Guid UniqueId { get; }
 
         /// <summary>
+        /// Gets the minimum version of the application required for compatibility.
+        /// </summary>
+        /// <remarks>
+        /// Feel free to use <see langword="null"/> or <see cref="string.Empty"/> if no minimum version is required.<br/>
+        /// However, if you specify a version, it should follow the format of <c>Major.Minor.Build.Revision</c>.<br/>
+        /// Host application will check its version before using the plugin features. And ask user to upgrade the application if not compatible.
+        /// </remarks>
+        /// <example>
+        /// 1.0.0.0
+        /// </example>
+        public string AppMinVersion { get; }
+
+        /// <summary>
         /// Get plugin name for UI.
         /// </summary>
         /// <remarks>
-        /// Do not return <see langword="null"/> or <see cref="string.Empty"/>.
+        /// For better user understanding. Please do not return <see langword="null"/> or <see cref="string.Empty"/>.
         /// </remarks>
         /// <param name="Locale">Locale code for localization.</param>
         /// <returns>Localized name.</returns>
@@ -30,7 +44,7 @@ namespace RX_Explorer_WAS.PluginDefinition
         /// Get plugin description for UI.
         /// </summary>
         /// <remarks>
-        /// Do not return <see langword="null"/> or <see cref="string.Empty"/>.
+        /// For better user understanding. Please do not return <see langword="null"/> or <see cref="string.Empty"/>.
         /// </remarks>
         /// <param name="Locale">Locale code for localization.</param>
         /// <returns>Localized name.</returns>
